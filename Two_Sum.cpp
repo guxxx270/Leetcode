f063453559b4f689a72da0542052e50bcfd982f9
Quanju Gu <quanjugu@Quanjus-MacBook-Pro.local>
2016-09-06 16:01:18 +0800

    LEETCODE

---
+++ Two_Sum.cpp
@@ -0,0 +1,28 @@
+//
+//  eval.cpp
+//  Homework2
+//
+//  Created by Quanju Gu on 7/12/16.
+//  Copyright © 2016 Jerry Gu. All rights reserved.
+//
+
+#include <string>
+#include <stack>
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+
+    }
+};
+
+/////test case///////////
+int main()
+{
+    //test for true
+    
+}
